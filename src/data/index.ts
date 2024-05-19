70f50208dac8c0ca1a8fb20b4f3ef7bbb64b8088
ChandraRaditya <chandraariwiraditya@gmail.com>
2024-05-20 01:28:26 +0700

    feat: add ts-node and dummydata

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import { IBooks } from '../interface/books';
+import dummyData from './dummyData.json';
 
-const books: IBooks[] = [];
+const books: IBooks[] = [...(dummyData as IBooks[])];
 
 export default books;
